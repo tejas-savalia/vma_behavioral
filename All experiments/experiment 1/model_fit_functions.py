fixing param space rerun init sign err

All experiments/experiment 1/model_fit_functions.py
@@ -94,11 +94,11 @@ def dual_state_model(As, Bs, Af, Bf, num_trials, p_type):
 
 def calc_log_likelihood(params, data, model, p_type, fit_type = 'regular', train_indices = None):
     if model == 'single state':
-        if any(params[:-1]) < 0 or any(params) > 1:
+        if any(params[:-1]) <= 0 or any(params) >= 1:
             return np.inf
         model_pred = single_state_model(params[0], params[1], len(data), p_type)
     else:
-        if any(params[:-1]) < 0 or any(params) > 1 or params[0] < params[2] or params[1] > params[3]:
+        if any(params[:-1]) <= 0 or any(params) >= 1 or params[0] <= params[2] or params[1] >= params[3]:
             return np.inf        
         model_pred = dual_state_model(params[0], params[1], params[2], params[3], len(data), p_type)
 
@@ -122,8 +122,10 @@ def fit_single(participant):
     print('participant started: ', participant)
 
     # try:
-    errors = data.loc[data['p_id'] == participant, 'avg errors'].values
+    errors = data.loc[data['p_id'] == participant, 'init signed error'].values
     p_type = data.loc[data['p_id'] == participant, 'Rotation'].unique()
+    print(len(errors))
+
     curr_fitval = np.inf
     possible_starting_points = itertools.product(np.linspace(0, 1, 8), np.linspace(0, 1, 8), np.linspace(0, 1, 8))
     for i in possible_starting_points:
@@ -140,17 +142,17 @@ def fit_single(participant):
 #load single fits to use as slow learning starting points.
 
 def fit_dual(participant):
-    single_fits = pd.read_csv('model_results/single_fit_avgerror_results.csv')
+    single_fits = pd.read_csv('model_results/single_fit_signed_initerror_results.csv')
     data = pd.read_csv('df_learn.csv')
 
     print('participant started: ', participant)
 
     try:
-        errors = data.loc[data['p_id'] == participant, 'avg errors'].values
+        errors = data.loc[data['p_id'] == participant, 'init signed error'].values
         p_type = data.loc[data['p_id'] == participant, 'Rotation'].unique()
         As_init = single_fits.loc[single_fits['p_id'] == participant, 'A'].values[0]
         Bs_init = single_fits.loc[single_fits['p_id'] == participant, 'B'].values[0]
-        # print(As_init)
+        print(len(errors))
 
         curr_fitval = np.inf
         possible_starting_points = itertools.product(np.linspace(0, 1, 8), np.linspace(0, 1, 8), np.linspace(0, 1, 8))
@@ -169,7 +171,7 @@ def fit_dual(participant):
 
 def fit_single_cv(participant, errors, p_type, train_indices, test_indices):
     # print('participant started: ', participant)
-    single_fits = pd.read_csv('model_results/single_fit_avgerror_results.csv')
+    single_fits = pd.read_csv('model_results/single_fit_signed_initerror_results.csv')
 
     starting_point = single_fits.loc[single_fits['p_id'] == participant, ['A', 'B', 'Eps']].values.tolist()       
     res = minimize(calc_log_likelihood, x0=starting_point, args=(errors, 'single state', p_type, 'cv', train_indices), bounds=((0, 1), (0, 1), (0, 1)), method = 'Nelder-Mead')
@@ -183,7 +185,7 @@ def fit_single_cv(participant, errors, p_type, train_indices, test_indices):
 #load single fits to use as slow learning starting points.
 
 def fit_dual_cv(participant, errors, p_type, train_indices, test_indices):
-    dual_fits = pd.read_csv('model_results/dual_fit_avgerror_results.csv')
+    dual_fits = pd.read_csv('model_results/dual_fit_signed_initerror_results.csv')
     # print('participant started: ', participant)
 
     # try:
@@ -199,7 +201,7 @@ def fit_dual_cv(participant, errors, p_type, train_indices, test_indices):
 def fit_cv(participant):
     data = pd.read_csv('df_learn.csv')
 
-    errors = data.loc[data['p_id'] == participant, 'avg errors'].values
+    errors = data.loc[data['p_id'] == participant, 'init signed error'].values
     p_type = data.loc[data['p_id'] == participant, 'Rotation'].unique()
     train_indices = np.sort(np.random.choice(np.arange(len(errors)), int(0.9*len(errors)), replace = False))
     test_indices = np.sort(np.delete(np.arange(len(errors)), train_indices)) 
@@ -209,7 +211,7 @@ def fit_cv(participant):
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('df_learnwashout_signed.csv')
+    # data = pd.read_csv('df_learnwashout_signed.csv')
 
     # participant = data['p_id'].unique()
     participant = np.arange(60)
@@ -220,7 +222,7 @@ if __name__ == '__main__':
 
     single_fit_results = pool.map(fit_single, participant)    
     df = pd.DataFrame(single_fit_results, columns =['p_id', 'gof', 'A', 'B', 'Eps'])
-    df.to_csv('model_results/single_fit_avgerror_results.csv')
+    df.to_csv('model_results/single_fit_signed_initerror_results.csv')
 
     dual_fit_results = pool.map(fit_dual, participant)    
     # dual_fit_results = []
@@ -228,7 +230,7 @@ if __name__ == '__main__':
     #     dual_fit_results.append(fit_dual(p))    
 
     df = pd.DataFrame(dual_fit_results, columns =['p_id', 'gof', 'As', 'Bs', 'Af', 'Bf', 'Eps'])
-    df.to_csv('model_results/dual_fit_avgerror_results.csv')
+    df.to_csv('model_results/dual_fit_signed_initerror_results.csv')
 
     single_fit_df = []
     dual_fit_df = []
@@ -247,10 +249,10 @@ if __name__ == '__main__':
         print('cv iteration done: ', i)
 
     df_full_single = pd.concat(single_fit_df)
-    df_full_single.to_csv('model_results/single_fit_avgerror_results_cv.csv', index = False)
+    df_full_single.to_csv('model_results/single_fit_signed_initerror_results_cv.csv', index = False)
 
     df_full_dual = pd.concat(dual_fit_df)
-    df_full_dual.to_csv('model_results/dual_fit_avgerror_results_cv.csv', index=False)
+    df_full_dual.to_csv('model_results/dual_fit_signed_initerror_results_cv.csv', index=False)
 
 
     # dual_fit_results = pool.map(fit_dual, participant)